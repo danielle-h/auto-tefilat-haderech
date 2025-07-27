0a63d96e88e3b1f6b222f2af2e7d98176623760d
Danielle Honigstein <dsavir.h@gmail.com>
2025-07-27 18:38:06 +0300

    Revert "feat: added sentry"
    This reverts commit dd228ef864e0b21b64b76dbe5bfec24947c5895a.

--- lib/main.dart
+++ lib/main.dart
@@ -5,26 +5,12 @@ import 'package:tefilat_haderech/constants.dart';
 import 'package:tefilat_haderech/l10n/app_localizations.dart';
 import 'package:tefilat_haderech/model/app_model_notifier.dart';
 import 'pages/home_page.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Alarm.init();
-  await SentryFlutter.init(
-    (options) {
-      options.dsn =
-          'https://ec27c468ef694ca4ad3e717e1da4fc51@o4509661316448256.ingest.de.sentry.io/4509740898713680';
-      // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
-      // We recommend adjusting this value in production.
-      options.tracesSampleRate = 0.5;
-      // The sampling rate for profiling is relative to tracesSampleRate
-      // Setting to 1.0 will profile 100% of sampled transactions:
-      options.profilesSampleRate = 0.5;
-    },
-    appRunner: () => runApp(SentryWidget(
-        child: ChangeNotifierProvider(
-            create: (context) => AppModelProvider(), child: const MyApp()))),
-  );
+  runApp(ChangeNotifierProvider(
+      create: (context) => AppModelProvider(), child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
