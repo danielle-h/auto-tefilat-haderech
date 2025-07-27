cbe285979ef8f95b6c3791cede7bf6e9fa7b097c
Danielle Honigstein <dsavir.h@gmail.com>
2025-07-27 17:20:25 +0300

    fix: background color is black in dark mode

--- lib/styles.dart
+++ lib/styles.dart
@@ -16,17 +16,11 @@ class AppTheme {
   static ThemeData darkTheme() {
     return ThemeData(
       colorScheme: ColorScheme.fromSeed(
-          surface: _deepAqua,
-          seedColor: _pastelGreen,
-          secondary: _deepAqua,
-          brightness: Brightness.dark),
-      //scaffoldBackgroundColor: Colors.black,
-      // textTheme: const TextTheme(
-      //   bodyLarge: TextStyle(color: Colors.white),
-      //   bodyMedium: TextStyle(color: Colors.white),
-      //   displayLarge: TextStyle(color: Colors.white, fontSize: 24),
-      //   displayMedium: TextStyle(color: Colors.white, fontSize: 18),
-      // ),
+              surface: _deepAqua,
+              seedColor: _pastelGreen,
+              secondary: _deepAqua,
+              brightness: Brightness.dark)
+          .copyWith(surface: Colors.black),
     );
   }
 }
